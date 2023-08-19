3b879933c5c09b70be288e8b9586670164ba68a3
Cristobal Andres Contreras Orellana <chrisloarryn@gmail.com>
2023-08-19 01:55:57 -0400

    feat: update first commit with very basic project structure

---
+++ src/test/java/com/chrisloarryn/demospringboot/DemoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisloarryn.demospringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
